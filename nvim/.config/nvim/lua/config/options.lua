3717356c05355a5771d41263c0d0578cf30017b2
r4ppz <johnreyrabosa.f@gmail.com>
2025-03-30 00:48:19 +0800

    update Neovim configuration to use spaces for indentation with a shift width of 4

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.expandtab = true  -- Use spaces instead of tabs
+vim.opt.shiftwidth = 4     -- Number of spaces for indentation
+vim.opt.tabstop = 4        -- Number of spaces for a tab
+vim.opt.softtabstop = 4    -- Number of spaces when pressing <Tab>
